54c9e0ac99c6c40f52d6035598ff0520da40fb7b
Israel Oliveira <102741887+IsraelOliver@users.noreply.github.com>
2022-06-24 09:17:57 -0300

    Add files via upload

--- btm/js/script.js
+++ btm/js/script.js
@@ -1,1 +1,16 @@
-'use strict'
+function add_btn(a) { 
+
+    if(cont < 8) { 
+        var form = document.querySelector('.person').cloneNode(true); 
+        document.querySelector('.person').after(form); } 
+    else { return 0; } 
+        return (++cont); } 
+    
+    function rmv_btn() { 
+        var form = document.querySelector('.person'); 
+        if(cont > 1) { form.remove(); --cont; } 
+        else 
+        return 0; 
+    }
+
+var cont = 1;
